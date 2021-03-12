935b7ebc7c6001160189f2eb9da727b89801bfdc
UO269911 <alexguirre.aa+UO269911@gmail.com>
2021-03-12 11:54:12 +0100

    Fix tests
    The change in `mobileapp\package.json` fixes a error "Unexpected token 'export'" in restapi-client when executing `npm test`.
    The change in `webapp\e2e\global-setup.js` allows to execute `npm run test:e2e` in Windows systems.

--- webapp/e2e/global-setup.js
+++ webapp/e2e/global-setup.js
@@ -14,7 +14,7 @@ module.exports = async () => {
         port: 5000,
     },
     {
-        command: 'BROWSER=none npm start',
+        command: process.platform === 'win32' ? 'SET BROWSER=none && npm start' : 'BROWSER=none npm start',
         launchTimeout: 60000,
         debug: true,
         port: 3000
--- webapp/src/components/LoginForm.test.js
+++ webapp/src/components/LoginForm.test.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import { render } from "@testing-library/react";
 import LoginForm from "./LoginForm";
+import { SessionContext } from '@inrupt/solid-ui-react';
 
 test('check that everything is rendering propertly', async () => {
-  const { getByText } = render(<LoginForm/>);
+  const { getByText } = render(
+    <SessionContext.Provider value={{sessionRequestInProgress: false}}>
+      <LoginForm/>
+    </SessionContext.Provider>);
   expect(getByText("Login")).toBeInTheDocument();
+});
+
+test('check that everything is rendering propertly while logging in', async () => {
+  const { getByText } = render(
+    <SessionContext.Provider value={{sessionRequestInProgress: true}}>
+      <LoginForm/>
+    </SessionContext.Provider>);
+  expect(getByText("Logging in...")).toBeInTheDocument();
 });
