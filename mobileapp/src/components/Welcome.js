77afe8769a59517054f64da20588d9445ce09ab6
Marina <uo257247@uniovi.es>
2021-03-13 11:59:28 +0100

    Trying to obtain the geolocation from the mobile app

--- mobileapp/src/components/Welcome.js
+++ mobileapp/src/components/Welcome.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
-import { getUsers } from 'restapi-client';
+import { addLocation, getUsers } from 'restapi-client';
 
 function Welcome(props) {
   return (
     <View>
       <Text style={styles.center}>Hi, {props.name}!</Text>
       <Button onPress={onButtonPress} title="Click Me" />
+      <Button onPress={getLocation} title = "Location"/>
     </View>
   );
 }
@@ -14,6 +15,13 @@ function Welcome(props) {
 async function onButtonPress() {
   const users = await getUsers();
   alert(JSON.stringify(users));
+}
+
+async function getLocation(){
+  navigator.geolocation.getCurrentPosition(function(position) {
+    console.log("Latitude is :", position.coords.latitude);
+    console.log("Longitude is :", position.coords.longitude);
+  });
 }
 
 const styles = StyleSheet.create({
