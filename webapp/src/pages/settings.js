2a482ad88722a3a46e0970f74a3130cbd4d35a6f
Marina <uo257247@uniovi.es>
2021-04-10 20:20:49 +0200

    Logic to delete a user from database

--- webapp/src/pages/settings.js
+++ webapp/src/pages/settings.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import UserList from '../components/UserList';
 
 
 class SettingsPage extends React.Component {
+
+
   render() {
     return (
       <div>
           <h2>Settings Page</h2>
+          
       </div>
     );
   }
