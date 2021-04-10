d81c35083f01bf23a80f64093c532b229c4f8df8
Alonso <UO269424@uniovi.es>
2021-04-11 01:42:34 +0200

    Didn't pull before, had a lot of unstaged changes

--- restapi/services/UsersService.js
+++ restapi/services/UsersService.js
@@ -1,11 +1,38 @@
 const User = require("../models/users")
+const FC = require("solid-file-client")
+const { Session } = require("@inrupt/solid-client-authn-node")
+const admins = ["https;//uo269911.inrupt.net/profile/card#me","https;//uo257247.inrupt.net/profile/card#me"]
 
+async function registerUser(webId) {
+    user = new User({
+        webId: webId,
+    })
+    await user.save()
+}
+
+async function isRegistered(webId) {
+    const user = await findByWebId(webId);
+    return user !== null;
+}
+
+function isAdmin(webId){
+    for(a in admins){
+        if (webId==a){
+            return true;
+        }
+    }
+    return false;
+}
 async function getAll() {
     return await User.find({}).sort('-_id') //Inverse order
 }
 
 async function findByWebId(webId) {
     return await User.findOne({ webId: webId })
+}
+
+async function deleteByWebId(webId) {
+    return await User.findOneAndRemove({ webId: webId })
 }
 
 async function addLocationToUser(userId, locationId) {
@@ -15,33 +42,73 @@ async function addLocationToUser(userId, locationId) {
     );
 }
 
-module.exports = {
-    getAll,
-    findByWebId,
-    addLocationToUser,
+async function updateUserLastLocation(session, latitude, longitude) {
+    const webId = session.info.webId;
+    if (!(await isRegistered(webId))) {
+        return;
+    }
+
+    const fc = new FC(session);
+    await createRadarinJsonFile(fc, webId, "lastLocation.json", { latitude, longitude })
+        .catch(err => {
+            console.log(`Failed to create 'lastLocation.json'`);
+            console.log(err);
+        });
 }
 
-import { FOAF } from "@inrupt/lit-generated-vocab-common";
-import { getSolidDataset, getThing, getStringNoLocale, getNamedNodeAll } from "@inrupt/solid-client";
+async function getUserLastLocation(session, webId) {
+    const fc = new FC(session ? session : new Session());
+    return await readRadarinJsonFile(fc, webId, "lastLocation.json");
+}
 
-async function printFriends(session) { // session provided by useSession() from @inrupt/solid-ui-react
-    const { webId } = session.info;
+async function createRadarinJsonFile(fc, webId, fileName, data) {
+    const fileUrl = getRadarinFolderPath(webId) + fileName;
 
-    // access our dataset
-    let profileDataset = await getSolidDataset(webId, { fetch: session.fetch });
-    let profile = getThing(profileDataset, webId);
+    await createRadarinFolder(fc, webId)
+    await fc.createFile(fileUrl, JSON.stringify(data), "application/json");
+}
 
-    // get the friends list
-    const knows = getNamedNodeAll(profile, FOAF.knows.iri.value);
-    for (const i in knows) {
-        const { id } = knows[i]; // get the friend's webId
+async function readRadarinJsonFile(fc, webId, fileName, data) {
+    const fileUrl = getRadarinFolderPath(webId) + fileName;
 
-        // access the friend's dataset
-        let friendProfileDataset = await getSolidDataset(id);
-        let friendProfile = getThing(friendProfileDataset, id);
-
-        // do something with the friend data
-        let friendName = getStringNoLocale(friendProfile, FOAF.name.iri.value);
-        console.log(friendName);
+    if (await fc.itemExists(fileUrl)) {
+        const json = await fc.readFile(fileUrl);
+        return JSON.parse(json);
     }
+
+    return null;
+}
+
+function getRadarinFolderPath(webId) {
+    return webId.replace("profile/card#me", "public/radarin_en2a/");
+}
+
+async function createRadarinFolder(fc, webId) {
+    const folderUrl = getRadarinFolderPath(webId);
+    if (await fc.itemExists(folderUrl)) {
+        return;
+    }
+
+    await fc.createFolder(folderUrl, {});
+
+    // TODO: setup permissions so only the user and its friends can access it by default
+    // currently anyone can access it because we are inside the public/ folder
+    // let aclUsers = await fc.acl.addUserMode({}, [{ agentClass: "Agent" }], ['Read', 'Write', 'Control'], ['accessTo'])
+    // aclUsers = await fc.acl.addUserMode(aclUsers, [{ agent: webId }], ['Read', 'Write', 'Control'], ['accessTo'])
+    // const aclContent = await fc.acl.createContent(folderUrl, [aclUsers])
+
+    // const { acl: aclUrl } = await fc.getItemLinks(folderUrl, { links: 'include_possible' });
+    // await fc.createFile(aclUrl, aclContent, 'text/turtle');
+}
+
+module.exports = {
+    registerUser,
+    isRegistered,
+    isAdmin,
+    getAll,
+    findByWebId,
+    deleteByWebId,
+    addLocationToUser,
+    updateUserLastLocation,
+    getUserLastLocation,
 }
