e21eab39e04bda354d2e8dc19565919ea3a91aef
Jorge Arias <uo271694@uniovi.es>
2021-04-11 21:52:34 +0200

    Added first delete locations unit test

--- restapi/tests/LocationsService.test.js
+++ restapi/tests/LocationsService.test.js
@@ -33,4 +33,38 @@ afterAll(async () => {
     await server.closeDB()
 })
 
+describe('LocationsService', () => {
+    it('can be deleted', async () => {
+        await UsersService.registerUser("https://uo271694.inrupt.net/profile/card#me");
+
+        const oviedo = { userWebId: user.webId, latitude: 43.36196825817341, longitude: -5.849390063878794 };
+        const gijon = { userWebId: user.webId, latitude: 43.53164223089106, longitude: -5.66129125890542 }
+
+        await request(app).post('/api/locations/add').send(gijon).set('Accept', 'application/json');
+
+
+        const added = await request(app).post('/api/locations/add').send(oviedo).set('Accept', 'application/json');
+        const oviedoId = added.body._id;
+
+        listResponse = await request(app).get('/api/locations/list')
+
+        //Check that the two locations were added correctly
+        expect(listResponse.body[0].latitude).toBe(oviedo.latitude);
+        expect(listResponse.body[0].longitude).toBe(oviedo.longitude);
+        expect(listResponse.body[1].latitude).toBe(gijon.latitude);
+        expect(listResponse.body[1].longitude).toBe(gijon.longitude);
+
+        //We remove oviedo
+        await request(app).get(`/api/locations/delete/${oviedoId}`);
+
+        //We get the new location list
+        listResponse = await request(app).get('/api/locations/list')
+
+        // We check that now the first location in the list is gijon as oviedo has been removed
+        expect(listResponse.body[0].latitude).toBe(gijon.latitude);
+        expect(listResponse.body[0].longitude).toBe(gijon.longitude);
+
+    });
+});
+
 
