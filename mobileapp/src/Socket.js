5fd2e7b6ffc8a1a3a7bd264b6f6f9f9da1ae4bc4
Alonso <UO269424@uniovi.es>
2021-04-11 23:00:28 +0200

    Possible solution achieved

--- mobileapp/src/Socket.js
+++ mobileapp/src/Socket.js
@@ -1,26 +1,60 @@
 
-let socket = new WebSocket("wss://javascript.info/article/websocket/demo/hello");
 
-socket.onopen = function (e) {
-    alert("[open] Connection established");
-    alert("Sending to server");
-    socket.send("My name is Alonso");
-};
+/*
+const express = require('express');
+const ws = require('ws');
+const { SessionContext } = require('./components/session/SessionContext');
+
+const app = express();
+
+/**
+ * ************************************************************************************************
+ */
+
+
+let socket = new WebSocket(process.env.REACT_APP_API_URI.replace("http","ws").replace("api",""));
 
 socket.onmessage = function (event) {
-    alert(`[message] Data received from server: ${event.data}`);
-};
-
-socket.onclose = function (event) {
-    if (event.wasClean) {
-        alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-    } else {
-        // e.g. server process killed or network down
-        // event.code is usually 1006 in this case
-        alert('[close] Connection died');
+    try {
+        alert(JSON.parse(event.data));
+    } catch (error) {
+        console.log("Error on receiving mesage from restapi")
     }
+    
 };
 
 socket.onerror = function (error) {
     alert(`[error] ${error.message}`);
 };
+
+export function sendRegisterMessage(sessionId)   {
+    socket.send(JSON.stringify({ type: "register", sessionId: sessionId }));
+}
+
+export function sendUnregisterMessage()  {
+    socket.send(JSON.stringify({ type: "unregister"}));
+}
+
+
+/*
+// Set up a headless websocket server that prints any
+// events that come in.
+const wsServer = new ws.Server({ server =  process.env.REACT_APP_API_URI});
+wsServer.on('connection', socket => {
+    socket.on('message', message => alert("[message] Data received from server: " + message));
+});
+
+
+
+
+// `server` is a vanilla Node.js HTTP server, so use
+// the same ws upgrade process described here:
+// https://www.npmjs.com/package/ws#multiple-servers-sharing-a-single-https-server
+const server = app.listen(3000);
+server.on('upgrade', (request, socket, head) => {
+    wsServer.handleUpgrade(request, socket, head, socket => {
+        wsServer.emit('connection', socket, request);
+    });
+});
+*/
+
