a8c48551a881fc40add7a635ade557817a9df661
Alonso <UO269424@uniovi.es>
2021-04-11 20:32:51 +0200

    Sends message from restapi

--- restapi/services/UsersService.js
+++ restapi/services/UsersService.js
@@ -155,6 +155,40 @@ function isNear(distance)   {
     return (distance <= maxDistance);
 }
 
+
+/**
+ * ****************************************************************************************************************************************************
+ * Notifications
+ * ****************************************************************************************************************************************************
+ */
+
+
+let socket = new WebSocket("wss://javascript.info/article/websocket/demo/hello");
+
+socket.onopen = function (e) {
+    alert("[open] Connection established");
+    alert("Sending to server");
+    socket.send("My name is Alonso");
+};
+
+socket.onmessage = function (event) {
+    alert(`[message] Data received from server: ${event.data}`);
+};
+
+socket.onclose = function (event) {
+    if (event.wasClean) {
+        alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+    } else {
+        // e.g. server process killed or network down
+        // event.code is usually 1006 in this case
+        alert('[close] Connection died');
+    }
+};
+
+socket.onerror = function (error) {
+    alert(`[error] ${error.message}`);
+};
+
 module.exports = {
     registerUser,
     isRegistered,
