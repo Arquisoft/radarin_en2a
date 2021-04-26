2c7ef517a66b68fdf0395d5deb01cf0b4d0aa99f
UO269911 <alexguirre.aa+UO269911@gmail.com>
2021-04-26 10:29:57 +0200

    Tweak mobileapp UI and add text explaining the options

--- mobileapp/src/components/Dashboard.js
+++ mobileapp/src/components/Dashboard.js
@@ -64,10 +64,24 @@ class Dashboard extends React.Component {
       }
     </View>
 
-    <View>
-      <Button onPress={this.getLocation.bind(this)} title = "Refresh Location"/>
-      <Button onPress={this.addLocation.bind(this)} title = "Save Location"/>
-      <Switch onValueChange={this.setIsSharingLocation.bind(this)} value={this.state.isSharingLocation}/>
+    <View style={styles.container}>
+      <View style={styles.singleComponentRow}>
+        <Button onPress={this.getLocation.bind(this)} title = "Refresh Location"/>
+      </View>
+      <View style={styles.singleComponentRow}>
+        <Button onPress={this.addLocation.bind(this)} title = "Save Location"/>
+      </View>
+      <View style={styles.singleComponentRow}>
+        <Text style={styles.helpText}>Save your current location. You can view and edit your saved locations through the Radaring web application.</Text>
+      </View>
+      <View style={styles.separator} />
+      <View style={styles.multiComponentRow}>
+        <Text style={styles.label}>Share Location</Text>
+        <Switch onValueChange={this.setIsSharingLocation.bind(this)} value={this.state.isSharingLocation} thumbColor="#2296F3" trackColor={{false: "#B2B2B2", true: "#79bbf2"}}/>
+      </View>
+      <View style={styles.singleComponentRow}>
+        <Text style={styles.helpText}>Enabling this option will share your location with your Solid Pod friends. You will receive notifications from nearby friends.</Text>
+      </View>
     </View>
     </>
   )}
@@ -76,6 +90,39 @@ class Dashboard extends React.Component {
 const styles = StyleSheet.create({
   center: {
     textAlign: "center",
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  singleComponentRow: {
+    justifyContent: 'space-between',
+    padding: 3,
+    marginHorizontal: 20,
+  },
+  multiComponentRow: {
+    flexGrow: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 3,
+    marginHorizontal: 20,
+  },
+  separator: {
+    borderWidth: 1.5,
+    borderColor: "#b0b0b0",
+    marginHorizontal: 16,
+    marginTop: 8,
+    marginBottom: 6
+  },
+  label: {
+    flexGrow: 1,
+    fontSize: 15,
+    textTransform: 'uppercase'
+  },
+  helpText: {
+    flexGrow: 1,
+    fontSize: 13
   },
 });
 
