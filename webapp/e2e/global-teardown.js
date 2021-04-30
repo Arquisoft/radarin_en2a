46fce7d0f2469d4256b017e0c475b877061fef0c
UO269911 <alexguirre.aa+UO269911@gmail.com>
2021-04-30 19:26:24 +0200

    Fix inconsistency in e2e tests
    By recreating incognito browser contexts instead of relaunching the whole browser

--- webapp/e2e/global-teardown.js
+++ webapp/e2e/global-teardown.js
@@ -1,5 +1,11 @@
-const { teardown: teardownDevServer } = require("jest-dev-server")
+const { teardown: teardownDevServer } = require("jest-dev-server");
+const fs = require('fs');
 
 module.exports = async () => {
-    await teardownDevServer()
-}
+    await teardownDevServer();
+
+    // cleanup Solid server data
+    if (fs.existsSync("./solid-server/data")) {
+        fs.rmdirSync("./solid-server/data", { recursive: true })
+    }
+};
