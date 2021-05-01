d14e67c042a1abf3e19b2aa02b8e7ee14ef7d5b0
Marina <uo257247@uniovi.es>
2021-05-02 01:04:11 +0200

    Login works with enter key

--- webapp/src/components/LoginForm.js
+++ webapp/src/components/LoginForm.js
@@ -3,6 +3,9 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { LoginButton, SessionContext } from "@inrupt/solid-ui-react";
 
+
+import './LoginForm.css'; // Tell webpack that Button.js uses these styles
+
 class LoginForm extends React.Component {
 
     constructor(props) {
@@ -22,8 +25,11 @@ class LoginForm extends React.Component {
       this.setState({idp: idp});
     }
 
-    setIdentityProvider(idp) {
-        this.setState({idp: idp});
+    handleKeypress(e) {
+        if (e.key === "Enter")
+        {
+            this.btn.click();
+        }
     }
 
     render() {
@@ -34,20 +40,25 @@ class LoginForm extends React.Component {
                 ?<span>Logging in...</span> 
                 :<>
                     <Form name="login">
+                        
                         <Form.Group>
                             <Form.Label>Identity Provider</Form.Label>
-                            <Form.Control name="idp" type="url" placeholder="Enter identity provider" onChange={this.changeIdentityProvider.bind(this)} value={this.state.idp} />
+                            <Form.Control className="input" name="idp" type="url" placeholder="Enter identity provider" 
+                            onKeyPress={this.handleKeypress.bind(this)}
+                            onChange={this.changeIdentityProvider.bind(this)} value={this.state.idp} />
                         
+                            <Form.Control className="smallButton" as="select" onChange={this.changeIdentityProvider.bind(this)} >
+                                <option value='https://inrupt.net'>Inrupt</option>
+                                <option value='https://solidcommunity.net'>Solid Community</option>
+                            </Form.Control>
                         </Form.Group>
-                        <>
-                        <Button onClick={() => this.setIdentityProvider('https://inrupt.net')}>Inrupt</Button>
-                        <Button onClick={() => this.setIdentityProvider('https://solidcommunity.net')}>Solid Community</Button>
-                        </>
-                        <LoginButton oidcIssuer={this.state.idp} redirectUrl={this.state.currentUrl}>
-                            <Button>
-                            Login
-                            </Button>
-                        </LoginButton>
+                        <div  className="singleComponentRow">
+                            <LoginButton oidcIssuer={this.state.idp} redirectUrl={this.state.currentUrl} >
+                                <Button className="button" type='button' variant="primary" ref={node => (this.btn = node)} >
+                                Login
+                                </Button>
+                            </LoginButton>
+                        </div>
                     </Form>
                 </>
             }
